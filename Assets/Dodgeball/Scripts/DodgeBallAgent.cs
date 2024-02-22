36752d21bfcd05ac8a1f68962ed24d07eb070dfc
Kamomilla <kmgodlun@stud.ntnu.no>
2024-02-22 18:25:59 +0100

    Changes to rule based agent v4

--- Assets/Dodgeball/Scripts/DodgeBallAgent.cs
+++ Assets/Dodgeball/Scripts/DodgeBallAgent.cs
@@ -952,11 +952,11 @@ public class DodgeBallAgent : Agent
                 break;
             case 4:
                 max_length = 50;
-                ball_interest = Mathf.Log(8, -currentNumberOfBalls + 5);
-                bush_interest = (float)0.4 * currentNumberOfBalls + (float)0.1;
+                ball_interest = Mathf.Log(9, -currentNumberOfBalls + 5);
+                bush_interest = (float)0.4 * currentNumberOfBalls + (float)0.2;
                 open_space_interest = x => (float)0.01; //-(x - (float)0.15) * (x - (float)0.15) * (float)10;
                 view_open_space_interest = (float)0.6;
-                agent_interest = Mathf.Exp(currentNumberOfBalls) / 100 - (float)0.01;
+                agent_interest = Mathf.Exp(currentNumberOfBalls) / 100; // Om trenger enklere: substraher mellom 0 og 0.01
                 agent_fear = currentNumberOfBalls > 2 ? (float)0 : (float)0.5;
                 rotation_in_movement_direction_interest = 0;
                 previous_movement_interest = 0;
--- Assets/Dodgeball/Scripts/DodgeBallPlayerAIHeuristic.cs
+++ Assets/Dodgeball/Scripts/DodgeBallPlayerAIHeuristic.cs
@@ -97,7 +97,7 @@ public class DodgeBallPlayerAIHeuristic : MonoBehaviour
         if (OnlyShootIfCanSeeTarget && canCurrentlySeeTarget && m_Agent)
         {
             float randomIndex = UnityEngine.Random.Range(0f, 1f);
-            if (randomIndex > 0.95f)
+            if (randomIndex > 0.93f)
             {
                 m_Agent.ThrowTheBall();
             }
